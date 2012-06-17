0d5ae72f4eb270e1acc3c5e48cbaf7b30a358dd9
paolotax <paolo.tassinari@gmail.com>
2012-06-17 21:11:31 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CroceviaSantacroce::Application.config.secret_token = '94f61bb62b108b31788ec88ccc94227f4a571327373f164ebf9b21ff819552d8fafae97cdd4c024564c681d1a9dc2f3f10308ee250ce8027d577990eaa59e40d'
