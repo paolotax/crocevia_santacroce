53b6222200a43418c0e239a78d4e7e4adefd805e
paolotax <paolo.tassinari@gmail.com>
2012-06-17 21:50:22 +0200

    deploy

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -1,7 +1,7 @@
 set_default(:postgresql_host, "localhost")
-set_default(:postgresql_user) { application }
+set_default(:postgresql_user) { 'crocevia' }
 set_default(:postgresql_password) { Capistrano::CLI.password_prompt "PostgreSQL Password: " }
-set_default(:postgresql_database) { "#{application}_production" }
+set_default(:postgresql_database) { "crocevia_production" }
 
 namespace :postgresql do
   desc "Install the latest stable release of PostgreSQL."
@@ -14,8 +14,8 @@ namespace :postgresql do
 
   desc "Create a database for this application."
   task :create_database, roles: :db, only: {primary: true} do
-    run %Q{#{sudo} -u postgres psql -c "create user crocevia with password '#{postgresql_password}';"}
-    run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner crocevia;"}
+    run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
+    run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
   end
   after "deploy:setup", "postgresql:create_database"
 
