a0eef95f2d50cf5370cfa88650449690ac5365d8
paolotax <paolo.tassinari@gmail.com>
2012-07-29 18:17:27 +0200

    cliente data_scadenza_text

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -9,11 +9,13 @@ class Cliente < ActiveRecord::Base
   
   attr_accessible :cap, :citta, :codice_fiscale, :cognome, :indirizzo, :nome,
                   :numero_tessera, :partita_iva, :provincia, :ragione_sociale, 
-                  :numero_documento, :data_rilascio_documento, :tipo_documento, 
+                  :numero_documento, :data_rilascio_documento_text, :tipo_documento, 
                   :documento_rilasciato_da, :telefono, :email, :cellulare, :note
-  
-  
-  
+
+  attr_writer :data_rilascio_documento_text
+  validate :check_data_rilascio_documento_text
+  before_save :save_data_rilascio_documento_text  
+
   def full_name
     [nome, cognome, ragione_sociale].join(" ")
   end
@@ -22,4 +24,22 @@ class Cliente < ActiveRecord::Base
     [cognome, nome].join(" ")
   end
   
+
+  def data_rilascio_documento_text
+    @data_rilascio_documento_text || data_rilascio_documento.try(:strftime, "%d-%m-%Y")
+  end
+
+  private
+    def save_data_rilascio_documento_text
+      self.data_rilascio_documento = Date.parse(@data_rilascio_documento_text) if @data_rilascio_documento_text.present?
+    end
+
+    def check_data_rilascio_documento_text
+      if @data_rilascio_documento_text.present? && Date.parse(@data_rilascio_documento_text).nil?
+        errors.add :data_rilascio_documento_text, "cannot be parsed"
+      end
+    rescue ArgumentError
+      errors.add :data_rilascio_documento_text, "data non valida"
+    end
+
 end
