d1cb37273e3c296bf566cb6b268f452401b48493
paolotax <paolo.tassinari@gmail.com>
2012-08-14 14:20:33 +0200

    prime modifiche documenti scaffold

--- app/controllers/documenti_controller.rb
+++ app/controllers/documenti_controller.rb
@@ -1,50 +1,33 @@
-
 class DocumentiController < ApplicationController
-  # GET /documenti
-  # GET /documenti.json
+
+  load_and_authorize_resource
+  
   def index
     @documenti = Documento.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @documenti }
-    end
   end
 
-  # GET /documenti/1
-  # GET /documenti/1.json
   def show
-    @documento = Documento.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @documento }
     end
   end
 
-  # GET /documenti/new
-  # GET /documenti/new.json
   def new
     @documento = Documento.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @documento }
     end
   end
 
-  # GET /documenti/1/edit
   def edit
-    @documento = Documento.find(params[:id])
   end
 
-  # POST /documenti
-  # POST /documenti.json
   def create
     @documento = Documento.new(params[:documento])
     
     if params[:documento][:tipo] == 'cassa'
-      # raise "GINO"
       @documento.add_movimenti_attivi(current_user)
     end
 
@@ -59,11 +42,7 @@ class DocumentiController < ApplicationController
     end
   end
 
-  # PUT /documenti/1
-  # PUT /documenti/1.json
   def update
-    @documento = Documento.find(params[:id])
-
     respond_to do |format|
       if @documento.update_attributes(params[:documento])
         format.html { redirect_to @documento, notice: 'Documento was successfully updated.' }
@@ -75,12 +54,8 @@ class DocumentiController < ApplicationController
     end
   end
 
-  # DELETE /documenti/1
-  # DELETE /documenti/1.json
   def destroy
-    @documento = Documento.find(params[:id])
     @documento.destroy
-
     respond_to do |format|
       format.html { redirect_to documenti_url }
       format.json { head :no_content }
