ef70450309f472b72f92d31e689d46e5c53a2821
paolotax <paolo.tassinari@gmail.com>
2012-08-23 17:04:41 +0200

    faild deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ CroceviaSantacroce::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( uploader.js uploader.css)
-
+  config.assets.precompile += ["uploader.js" ,"uploader.css"]
+  
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
