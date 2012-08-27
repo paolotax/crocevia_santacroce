10841cdabf71c88de5df8671272aeb22cba8b4d7
paolotax <paolo.tassinari@gmail.com>
2012-08-27 09:23:43 +0200

    Devise initializer hack for exiting user (not good but works)

--- config/routes.rb
+++ config/routes.rb
@@ -43,15 +43,17 @@ CroceviaSantacroce::Application.routes.draw do
   end
   root :to => "home#index"
   
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   # 
   # devise_scope :user do
   #   get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
   # end
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   
-  resources :users, path: "/utenti", :only => [:show, :index, :update]
+  resources :users, :only => [:show, :index, :update]
+
 
   match '/regolamento', to: 'home#regolamento'
   match '/staff',       to: 'home#staff'
