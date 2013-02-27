8326bf6869d6b7d23231363b7821d19367523bcc
paolotax <paolo.tassinari@gmail.com>
2013-02-27 18:33:41 +0100

    recipes for digital ocean 12.10

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -6,7 +6,6 @@ set_default(:postgresql_database) { "crocevia_production" }
 namespace :postgresql do
   desc "Install the latest stable release of PostgreSQL."
   task :install, roles: :db, only: {primary: true} do
-    run "#{sudo} add-apt-repository ppa:pitti/postgresql"
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install postgresql-9.1 libpq-dev postgresql-contrib-9.1"
   end
