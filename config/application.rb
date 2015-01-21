f04fb5f603043ebd92011cf8f92a87fac1e0211e
Paolo Tassinari <paolo.tassinari@gmail.com>
2015-01-21 19:50:09 +0100

    notify mail

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,11 @@ module CroceviaSantacroce
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :it
+    
+    # fix locale in production ??? 
     config.i18n.locale = :it
+    
+    
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
