48140e39b6e7e42169fd11305ea3f5f0f34cce83
Paolo Tassinari <paolo.tassinari@gmail.com>
2015-02-13 19:12:29 +0100

    scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,3 +3,10 @@ set :output, "#{path}/log/cron.log"
 every 1.day, at: '5.30am' do
   runner "Articolo.ricalcola_eli"
 end
+
+
+every 1.day, at: '8.30pm' do
+  runner "Notifier.riepilogo_giornata.deliver"
+end
+
+
