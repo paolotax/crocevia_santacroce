376c5262680a14e7e820ef89a1a436d5cfe034f9
Paolo Tassinari <paolo.tassinari@gmail.com>
2015-02-14 09:16:54 +0100

    new schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,9 +4,6 @@ every 1.day, at: '5.30am' do
   runner "Articolo.ricalcola_eli"
 end
 
-
-every 1.day, at: '8.30pm' do
+every 1.day, at: '7.30pm' do
   runner "Notifier.riepilogo_giornata.deliver"
 end
-
-
